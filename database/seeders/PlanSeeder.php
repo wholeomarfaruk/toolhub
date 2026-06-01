2f3c43a67d28f082b205ad48b4ae86db6e622bc3
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-06-02 02:03:46 +0600

    Update plan pricing: Pro \.00/month, Enterprise \.99/month

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -25,8 +25,8 @@ class PlanSeeder extends Seeder
             [
                 'name'           => 'Pro',
                 'description'    => 'Professional tools with PDF export and invoice saving',
-                'price_monthly'  => 299,         // $2.99
-                'price_yearly'   => 2990,        // ~$249.99 annual (discounted)
+                'price_monthly'  => 1200,        // $12.00
+                'price_yearly'   => 11952,       // $119.52 annual (17% discount)
                 'is_active'      => true,
                 'sort_order'     => 2,
             ]
@@ -37,8 +37,8 @@ class PlanSeeder extends Seeder
             [
                 'name'           => 'Enterprise',
                 'description'    => 'Full platform access with team collaboration and API',
-                'price_monthly'  => 799,         // $7.99
-                'price_yearly'   => 7990,        // ~$665.83 annual (discounted)
+                'price_monthly'  => 1599,        // $15.99
+                'price_yearly'   => 15926,       // $159.26 annual (17% discount)
                 'is_active'      => true,
                 'sort_order'     => 3,
             ]
